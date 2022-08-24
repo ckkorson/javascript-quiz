5f3e391a0ff62793c25db07d12a028a9fbb9d809
Caleb Korson <caleb@Calebs-Air.attlocal.net>
2022-08-24 17:54:04 -0400

    added comments to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// declare variables that coorespond to different areas of the page, ids, or classes
 let allButton = document.querySelector(".all-button")
 let button1 = document.querySelector("#button1")
 let main = document.querySelector("main")
@@ -5,18 +6,20 @@ let questionNumber = document.querySelector("#main-header")
 let questionText = document.querySelector("#main-text")
 let timer = document.querySelector("#time-left")
 let viewHighScore = document.querySelector("#high-score")
+// create three new buttons and assign them to a variall
 let button2 = document.createElement("button")
 let button3 = document.createElement("button")
 let button4 = document.createElement("button")
-document.getElementById("initial-form").style.display = "none"
-// let intialForm = document.createElement("form")
+// create set inital value of variable to be used later in js
+// gameStart variable will be used through the code to track button presses
 let rightAnswer = 0
-let wrongAnswer = 0
 let gameStart = 0
 let timeLeft = 0
-
 let x = 0
-
+// make the form element not visible yet
+document.getElementById("initial-form").style.display = "none"
+// create object for the quiz questions
+// object includes question #, question, 4 different answers, and the correct answer
 let questions = {
     qNum: [1,2,3],
     qText: ["Who is the best coder?","What do you use to declare arrays?",
@@ -27,7 +30,9 @@ let questions = {
     optionD: ["Brewer","||",".appendChild"],
     correct: ["Caleb","[]",".setAttribute"],
 }
-
+// creates the timer countdown function
+// countdown starts and 10 seconds and counts down in 1 second intervals
+// timeInterval is cleared either by answering all question or running out of time
 function countdown() {
     timeLeft = 10
     timer.innerHTML = timeLeft
@@ -43,7 +48,7 @@ function countdown() {
       }
     },1000)
   }
-
+// create function to start the game and add the question and the new buttons for answers
 function addQuestions() {
     viewHighScore.style.display = "none"
     main.appendChild(button2)
@@ -64,7 +69,7 @@ function addQuestions() {
     gameStart++
     x++
 }
-
+// replaces the last question with a new question
 function newQuestions() {
     questionNumber.innerHTML = ("Question " + questions.qNum[x])
     questionText.innerHTML = questions.qText[x]
@@ -74,7 +79,8 @@ function newQuestions() {
     button4.innerHTML = questions.optionD[x]
     x++
 }
-
+// function to tell user that quiz is complete
+// this will run when user completes all question or runs out of time
 function gameOver() {
     button2.remove()
     button3.remove()
@@ -84,18 +90,16 @@ function gameOver() {
     button1.innerHTML = "Next"
     gameStart = 2
 }
-
+// function to show the user their score on the quiz and give them a chance to record the initials
 function showScore() {
     questionNumber.innerHTML = ("Your Score is: " + rightAnswer + " out of " + questions.qNum.length)
     questionText.innerHTML = "Enter your initials in textbox below to record you score, then press submit"
     button1.innerHTML = "Submit"
-    // if (localStorage.getItem("highScore") <= rightAnswer) {
-    //     localStorage.setItem("highScore",rightAnswer)
-    // }
     document.getElementById("initial-form").style.display = "flex"
     gameStart++
 }
-
+// this function resets the quiz back to start page
+// it also checks to see if the user scored a new high score and records it in loca storage if they did
 function resetQuiz() {
     if (localStorage.getItem("highScore") <= rightAnswer) {
         localStorage.setItem("highScore",rightAnswer)
@@ -113,7 +117,7 @@ function resetQuiz() {
     timer.innerHTML = 0
     document.getElementById("input-text").value = ""
 }
-
+// adds eventListeners to button1 and establishes what to do based off the gameStart variable
 button1.addEventListener("click", function() {
     if (gameStart == 2) {
         showScore()
@@ -143,7 +147,7 @@ button1.addEventListener("click", function() {
         newQuestions()
     }
 })
-
+// adds eventListeners to button2 and establishes what to do based off the gameStart variable
 button2.addEventListener("click", function() {
     if (x == questions.qNum.length) {
         x--
@@ -164,7 +168,7 @@ button2.addEventListener("click", function() {
         newQuestions()
     }
 })
-
+// adds eventListeners to button3 and establishes what to do based off the gameStart variable
 button3.addEventListener("click", function() {
     if (x == questions.qNum.length) {
         x--
@@ -184,7 +188,7 @@ button3.addEventListener("click", function() {
         newQuestions()
     }
 })
-
+// adds eventListeners to button4 and establishes what to do based off the gameStart variable
 button4.addEventListener("click", function() {
     if (x == questions.qNum.length) {
         x--
@@ -204,7 +208,7 @@ button4.addEventListener("click", function() {
         newQuestions()
     }
 })
-
+// add an event listener to the View high score text that will show the user the current high score.
 viewHighScore.addEventListener("click", function() {
     questionNumber.innerHTML = "Current High Score"
     questionText.innerHTML = (localStorage.getItem("highScore") + " out of " + questions.qNum.length +
